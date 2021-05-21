840efeabfb07b5adcf222eb11b58400b794fdf10
madmajor <madmajor@github.com>
2021-05-21 17:53:00 +0900

    README.md update

--- 05_backtesting_test.py
+++ 05_backtesting_test.py
@@ -1,8 +1,7 @@
 from backtesting import Backtest, Strategy
 from backtesting.lib import crossover
-
 from backtesting.test import SMA, GOOG
-
+import FinanceDataReader as fdr
 
 class SmaCross(Strategy):
     n1 = 10
@@ -20,10 +19,22 @@ class SmaCross(Strategy):
             self.sell()
 
 
+# 셀트리온, 2018년~2019년6월
+data = fdr.DataReader('068270', '20180104','20190630')
+print(data.head())
+
+# 초기투자금 10000, commission 비율 0.002 임의 지정
+bt = Backtest(data, SmaCross,
+              cash=10000, commission=.002,
+              exclusive_orders=True)
+
+'''
 bt = Backtest(GOOG, SmaCross,
               cash=10000, commission=.002,
               exclusive_orders=True)
+'''
 
 output = bt.run()
 print(output)
 bt.plot()
+
---
+++ README.md
@@ -0,0 +1,17 @@
+## Dependencies
+Ta-Lib Source Download [ta-lib-0.4.0-src.tar.gz](https://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz)
+```bash
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
+```
+
+```bash
+pip install wheel
+pip install BeautifulSoup4
+pip install TA-Lib
+pip install backtesting
+pip install finance-datareader
+```
